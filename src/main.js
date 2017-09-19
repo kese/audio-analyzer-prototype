a8d27b5a6915c5aaf270a4297823e776ab0c3eda
Sebastian Kehr <kese@users.noreply.github.com>
2017-09-19 10:42:29 +0200

    adjusted build command

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ app.on('ready', function() {
     const win = new BrowserWindow({
         'width': 1024, 'height': 768,
         'auto-hide-menu-bar': true,
-        'title': 'Viewer Prototype - Revision 2015-12-08 - Peter Holstein, Sebastian Kehr',
+        'title': 'Audio Analyzer Prototype - Sebastian Kehr',
         'web-preferences': { 'webaudio': true }
     })
 
